5d519c9005e946379c9d4e508078dbb2ae60c394
zGiusee <giusebelforte@gmail.com>
2024-03-19 13:15:58 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script>
+import axios from 'axios';
+export default {
+
+}
+</script>
+<template>
+  <router-view></router-view>
+</template>
+<style lang="scss">
+@use './styles/generals.scss' as *;
+</style>
---
+++ src/pages/AppHome.vue
@@ -0,0 +1,12 @@
+<script>
+
+export default {
+
+}
+</script>
+<template>
+    <div>
+        <h1>ciao</h1>
+    </div>
+</template>
+<style scoped lang="scss"></style>
