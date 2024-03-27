58fa27f1bb919d2813c9f80210197db035d5561d
moonymel <musumeci.me@gmail.com>
2024-03-27 14:40:16 +0100

    [UPDATE] fixed header responsive

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,20 @@
 <script>
+import { ref, onMounted } from 'vue';
+import { Dropdown } from 'bootstrap';
+
 export default {
+
+    setup() {
+        const dropdownButton = ref(null);
+
+        onMounted(() => {
+            // Inizializza il dropdown utilizzando l'API JavaScript di Bootstrap
+            new Dropdown(dropdownButton.value);
+        });
+
+        return { dropdownButton };
+    },
+
     data() {
         return {
             navLinks: [
@@ -28,18 +43,33 @@ export default {
                 </div>
 
                 <!-- NAV LINKS  -->
-                <div class="col-12 col-sm-4 d-flex align-items-center justify-content-center">
-                    <ul class="p-0">
-                        <li v-for="(link, index) in navLinks">
+                <div class="col-10 col-sm-4 d-flex align-items-center justify-content-center">
+                    <ul class="menu p-0">
+                        <li v-for="(link, index) in navLinks" class="menu">
                             <router-link :to="{ name:link.name }"> {{ link.label }}</router-link>
                         </li>
                     </ul>
                 </div>
 
-                <!-- BOTTONI  -->
-                <div class="col-12 col-sm-4 d-flex align-items-center justify-content-center justify-content-sm-end">
+                <!-- BOTTONI (COMPUTER) -->
+                <div class="d-none col-lg-4 d-lg-flex align-items-center justify-content-lg-end">
                     <button class="light-button me-3">Login</button>
                     <button class="dark-button">Create an account</button>
+                </div>
+
+                <!-- BOTTONI (MOBILE & TABLET) -->
+                <div class="col-2 col-sm-4 text-end align-self-center d-lg-none">
+
+                    <div class="btn-group my-dropdown">
+                        <button type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-list"></i>
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-end text-end">
+                            <li><a class="dropdown-item" href="#">Login</a></li>
+                            <li><a class="dropdown-item" href="#">Create an account</a></li>
+                        </ul>
+                    </div>
+
                 </div>
 
             </div>
@@ -56,25 +86,39 @@ export default {
     background-color: $my_lightblue;
     color: #fff;
 
-    ul {
+    a {
+        color: #fff;
+        text-decoration: none;
+    }
+
+    ul.menu {
         list-style: none;
         font-size: 17px;
         margin: 0px;
         display: flex;
 
-        li {
+        li.menu {
             padding: 0px 15px;
-
-            a {
-                color: #fff;
-                text-decoration: none;
-            }
         }
     }
 
     .logo {
         filter: brightness(0) invert(1);
         width: 120px;
+    }
+
+    .my-dropdown {
+
+        button {
+            background-color: $my_lightblue;
+            border: none;
+            color: white;
+            font-size: 20px;
+        }
+
+        a {
+            color: black;
+        }
     }
 }
 </style>
