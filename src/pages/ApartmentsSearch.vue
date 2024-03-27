6c6616712220e43533d06420f535504ab3ccda32
anto24s96 <anto24s96@gmail.com>
2024-03-27 18:45:28 +0100

    [UPDATE] added apartments in home

--- src/pages/ApartmentsSearch.vue
+++ src/pages/ApartmentsSearch.vue
@@ -14,32 +14,11 @@ export default {
     data() {
         return {
             store,
-            apartments: null,
         }
     },
-    created() {
-        this.getApartments();
-    },
-    methods: {
-        getApartments(query) {
-            axios.get(`${this.store.endpoint}/api/apartments/${this.$route.params.query}`).then((response) => {
-                this.apartments = response.data.results;
-                console.log(this.apartments)
-            })
-        }
-    },
+
 }
 </script>
 <template>
-    <AppHeader />
-    <AppJumbotron />
-    <div>
-        <div v-for="apartment in apartments">
-            {{ apartment.title }}
-            {{ apartment.rooms }}
-            {{ apartment.address }}
-        </div>
-    </div>
-    <AppFooter />
 </template>
 <style scoped lang="scss"></style>
