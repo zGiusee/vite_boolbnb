1ede7e907690aebcd7b7755e3a3517ef1eec4f4f
zGiusee <giusebelforte@gmail.com>
2024-03-28 15:53:28 +0100

    Search filter done

--- src/pages/ApartmentsSearch.vue
+++ src/pages/ApartmentsSearch.vue
@@ -17,10 +17,10 @@ export default {
         return {
             store,
             query: ' ',
-            beds: ' ',
-            rooms: ' ',
-            radius: ' ',
-            bathrooms: ' ',
+            beds: 0,
+            rooms: 0,
+            radius: 20,
+            bathrooms: 0,
             apartments: null,
         }
     },
@@ -65,6 +65,37 @@ export default {
             }).catch((error) => {
                 console.log('error');
             })
+        },
+        // COUNTERS
+        bathroomsIncrement() {
+            if (this.bathrooms < 99) {
+                this.bathrooms++;
+            }
+        },
+        bathroomsDecrement() {
+            if (this.bathrooms > 0) {
+                this.bathrooms--;
+            }
+        },
+        bedsIncrement() {
+            if (this.beds < 99) {
+                this.beds++;
+            }
+        },
+        bedsDecrement() {
+            if (this.beds > 0) {
+                this.beds--;
+            }
+        },
+        roomsIncrement() {
+            if (this.rooms < 99) {
+                this.rooms++;
+            }
+        },
+        roomsDecrement() {
+            if (this.rooms > 0) {
+                this.rooms--;
+            }
         }
     },
 
@@ -78,37 +109,89 @@ export default {
     <div class="container my-5">
 
         <!-- SEARCHBAR  -->
-        <div class="row justify-content-center text-center">
-            <div class="popular-title mb-3">
+        <div class="row search-container justify-content-center">
+            <div class="popular-title mb-3 text-center">
                 Search your destination
                 <div class="popular-subtitle">
                     find the perfect avaible apartment for your trip
                 </div>
             </div>
-            <div class="col-12 col-sm-10 col-lg-6 d-flex">
-                <input type="text" v-model="query" class="form-control" name="query" id="query">
-                beds:
-                <input type="text" v-model="beds" class="form-control" name="beds" id="beds">
-                rooms:
-                <input type="text" v-model="rooms" class="form-control" name="rooms" id="rooms">
-                radius:
-                <input type="text" v-model="radius" class="form-control" name="radius" id="radius">
-                bathrooms:
-                <input type="text" v-model="bathrooms" class="form-control" name="bathrooms" id="bathrooms">
-                <button type="button" class="search-button ms-2"
-                    @click="getApartmentsRefresh(query, beds, rooms, radius, bathrooms)">Search</button>
+
+            <!-- ADRESS INPUT -->
+            <div class="col-12 d-flex justify-content-center mt-4 search-container">
+                <label for=" query">Address</label>
+                <input type="text" class="w-50" v-model="query" name="query" id="query">
+            </div>
+
+            <!-- RADIUS SLECET INPUT -->
+            <div class="col-6 col-md-3 mt-4 d-flex justify-content-center align-items-end">
+                <div>
+                    <label for="radius" class=" d-block d-lg-inline-block ">Radius</label>
+                    <select name="radius" v-model="radius" id="radius">
+                        <option value="1">1 km</option>
+                        <option value="2">2 km</option>
+                        <option value="3">3 km</option>
+                        <option value="4">4 km</option>
+                        <option value="5">5 km</option>
+                        <option value="10">10 km</option>
+                        <option value="20" selected>20 km</option>
+                    </select>
+                </div>
+            </div>
+
+            <!-- BEDS INPUT -->
+            <div class="col-6 col-md-3 mt-4 d-flex justify-content-center ">
+                <div class="counter">
+                    <label for="Beds" class=" d-block d-lg-inline-block ">Beds</label>
+                    <div class="d-inline-block input-container">
+                        <button type="button" @click="bedsDecrement"><span
+                                :class="beds == 0 ? 'my-disabled' : ''">-</span></button>
+                        <input type="text" min="0" max="99" size="1" maxlength="2" v-model="beds" name="beds" id="beds">
+                        <button type="button" @click="bedsIncrement"><span
+                                :class="beds == 99 ? 'my-disabled' : ''">+</span></button>
+                    </div>
+                </div>
+            </div>
+
+            <!-- ROOMS INPUT -->
+            <div class="col-6 col-md-3 mt-4 d-flex justify-content-center  ">
+                <div class="counter">
+                    <label for="rooms" class=" d-block d-lg-inline-block ">Rooms</label>
+                    <div class="d-inline-block input-container">
+                        <button type="button" @click="roomsDecrement"><span
+                                :class="rooms == 0 ? 'my-disabled' : ''">-</span></button>
+                        <input type="text" min="0" max="99" size="1" maxlength="2" v-model="rooms" name="rooms"
+                            id="rooms">
+                        <button type="button" @click="roomsIncrement"><span
+                                :class="rooms == 99 ? 'my-disabled' : ''">+</span></button>
+                    </div>
+                </div>
+            </div>
+
+            <!-- BATHROOMS INPUT -->
+            <div class="col-6 col-md-3 mt-4 d-flex justify-content-center ">
+                <div class="counter">
+                    <label for="bathrooms" class=" d-block d-lg-inline-block ">Bathrooms</label>
+                    <div class="d-inline-block input-container">
+                        <button type="button" @click="bathroomsDecrement"><span
+                                :class="bathrooms == 0 ? 'my-disabled' : ''">-</span></button>
+                        <input type="text" min="0" max="99" size="1" maxlength="2" v-model="bathrooms" name="bathrooms"
+                            id="bathrooms">
+                        <button type="button" @click="bathroomsIncrement"><span
+                                :class="bathrooms == 99 ? 'my-disabled' : ''">+</span></button>
+                    </div>
+                </div>
+            </div>
+
+            <!-- ADRESS BUTTON -->
+            <div class="col-12 d-flex justify-content-center mt-4">
+                <div>
+                    <button type="button" class="search-button"
+                        @click="getApartmentsRefresh(query, beds, rooms, radius, bathrooms)">Search
+                    </button>
+                </div>
             </div>
         </div>
-
-        <!-- <div class="row justify-content-center align-items-end my-3">
-    <div class="col-6">
-        <label for="query" class="d-block mb-2">Search your destination</label>
-        <input type="text" v-model="query" class="form-control" name="query" id="query">
-    </div>
-    <div class="col-6">
-        <router-link class="dark-button" :to="{ name: 'search', params: { query: query } }">Search</router-link>
-    </div>
-</div> -->
 
         <!-- CARD CICLATE  -->
         <div class="row column-gap-1 justify-content-around mt-5 justify-content-center">
@@ -116,23 +199,6 @@ export default {
         </div>
     </div>
 
-
-
-    <!-- <div class="container">
-        <div class="row align-items-end  my-3">
-            <div class="col-6">
-                <label for="query" class="d-block mb-2">Search a destination</label>
-                <input type="text" v-model="query" class="form-control" name="query" id="query">
-            </div>
-            <div class="col-6 d-flex">
-                <button type="button" class="search-button ms-2" @click="getApartments2(query)">Search</button>
-            </div>
-            <div class="row column-gap-1 justify-content-around mt-5 justify-content-center">
-
-                <ApartmentCard v-for="apartment in apartments" :apartment="apartment" />
-            </div>
-        </div>
-    </div> -->
     <AppFooter />
 </template>
 <style scoped lang="scss">
@@ -144,5 +210,67 @@ export default {
     .popular-subtitle {
         font-size: 20px;
     }
+}
+
+.my-disabled {
+    color: rgb(202, 202, 202);
+    cursor: default;
+}
+
+.search-container {
+    font-size: 18px;
+
+    label {
+        margin-right: 10px;
+    }
+
+    //STILE PER LA SEARCHBOX
+    .search-container {
+        input {
+            padding: 5px 0px;
+            border: 1px solid $my_lightblue;
+
+            &:focus {
+                outline: none;
+            }
+        }
+
+    }
+
+    // STILE PER LA SELECT 
+    select {
+        padding: 7px 20px;
+        background-color: white;
+        border: 1px solid $my_lightblue;
+    }
+
+    // STILE PER I COUNTER
+    .counter {
+        .input-container {
+            border: 1px solid $my_lightblue;
+            padding: 7px 14px;
+
+            button {
+                border: none;
+                background-color: transparent;
+                color: $my_lightblue;
+                cursor: pointer;
+                font-size: 20px;
+            }
+        }
+
+        input {
+            width: 23px;
+            border: 0;
+            margin-left: 10px;
+            margin-right: 4px;
+
+            &:focus {
+                outline: none;
+            }
+        }
+    }
+
+    // STILE BOTTONE
 }
 </style>
