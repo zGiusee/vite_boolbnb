6e7e58f82ff2e55d2b55879ca5f340223e9b4a0e
zGiusee <giusebelforte@gmail.com>
2024-03-29 14:17:37 +0100

    Message done

--- src/pages/ApartmentDetail.vue
+++ src/pages/ApartmentDetail.vue
@@ -6,6 +6,11 @@ export default {
         return {
             store,
             apartment: null,
+            name: '',
+            email: '',
+            apartment_id: '',
+            description: '',
+            errors: '',
             error: '',
         }
     },
@@ -24,6 +29,34 @@ export default {
             }).catch((error) => {
                 this.error = 'There are no apartments matching the entered parameters...'
             })
+        },
+        sendMessage() {
+            // Definisco i dati che verranno passati al BE
+            const data = {
+                apartment_id: this.apartment.id,
+                name: this.name,
+                email: this.email,
+                description: this.description,
+                date: new Date().toISOString().substring(0, 10),
+            }
+
+            // Faccio la chiamata passando i dati
+            axios.post(`${this.store.endpoint}/api/contact`, data).then((response) => {
+
+                if (response.data.succes) {
+                    this.name = '';
+                    this.email = '';
+                    this.description = '';
+
+                    // this.$router.push({
+                    //     name: 'thank-you',
+                    // })
+
+                } else {
+                    this.errors = response.data.errors
+                }
+
+            })
         }
     },
 }
@@ -36,6 +69,40 @@ export default {
     </div>
     <div v-else>
         {{ apartment.title }}
+
+        <form @submit.prevent="sendMessage()" method="post">
+            <div class="row justify-content-center py-5">
+                <div class="col-6">
+                    <div class="row">
+                        <div class="col py-3">
+                            <input v-model="name" type="text" name="name" id="name" class="form-control"
+                                placeholder="First name" required>
+                            <p v-for="(error, index) in errors.name" :key="`message-error-${index}`"
+                                class="text-danger">
+                                {{ error }} </p>
+
+                        </div>
+                        <div class="col py-3">
+                            <input type="text" v-model="email" class="form-control" placeholder="Email" name="email"
+                                id="email" required>
+                            <p v-for="(error, index) in errors.email" :key="`message-error-${index}`"
+                                class="text-danger">
+                                {{ error }}</p>
+                        </div>
+                        <div class="py-3">
+
+                            <label for="description" class="form-label">Description</label>
+                            <textarea class="form-control" v-model="description" name="description" id="description"
+                                rows="3"></textarea>
+                            <p v-for="(error, index) in errors.description" :key="`message-error-${index}`"
+                                class="text-danger">{{ error }}</p>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary"> Invia</button>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
 </template>
 <style scoped lang="scss">
